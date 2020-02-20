5f0dbcd6cf768312cca4f4c47a52e42402a82d86
Stoncs <andrej2002322@gmail.com>
2020-02-20 22:33:47 +0300

    Тесты написаны

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,7 +1,8 @@
 public class TicTacToe {
     String[][] field;
-
+    private int desk;
     public TicTacToe(int n){
+        desk = n;
         int i, j;
         field = new String[n][n];
         for (i = 0; i < n; ++i) {
@@ -10,10 +11,10 @@ public class TicTacToe {
         }
     }
 
-    private void show(int n){
+    private void show(){
         int i, j;
-        for (i = 0; i < n; ++i) {
-            for (j = 0; j < n; ++j)
+        for (i = 0; i < desk; ++i) {
+            for (j = 0; j < desk; ++j)
                 System.out.print(field[i][j] + "  ");
             System.out.println();
         }
@@ -27,18 +28,18 @@ public class TicTacToe {
         field[y - 1][x - 1] = "X";
     }
 
-    public void Delete(int x, int y) {
+    public void delete(int x, int y) {
         field[y - 1][x - 1] = "_";
     }
 
-    public int checkCross(int n) {
+    public int checkCross() {
         int current, i, j, m;
         int max = 0;
-        for (i = 0; i < n; i++) {               //проверка горизонталей, если нет ни одного?
+        for (i = 0; i < desk; i++) {               //проверка горизонталей, если нет ни одного?
             if (field[i][0].equals("X"))
             current = 1;
              else current = 0;
-            for (j = 1; j < n; j++)
+            for (j = 1; j < desk; j++)
             if (!field[i][j].equals("X")) {
                 if (current > max) max = current;
                 current = 0;
@@ -47,33 +48,32 @@ public class TicTacToe {
             if (current > max) max = current;
         }
         if (max == 0) return max;
-        for (j = 0; j < n; j++) {               //проверка вертикалей
+        for (j = 0; j < desk; j++) {               //проверка вертикалей
             if (field[0][j].equals("X")) current = 1;
             else current = 0;
-            for (i = 1; i < n; i++) {
-                if (field[i][j].equals("X")) {
+            for (i = 1; i < desk; i++) {
+                if (!field[i][j].equals("X")) {
                     if (current > max) max = current;
                     current = 0;
                 } else current += 1;
             }
             if (current > max) max = current;
         }
-        for (i = 0; i < n; i++) {               //проверка одной диагонали и || ей (первая половина)
+        for (i = 0; i < desk; i++) {               //проверка одной диагонали и || ей (первая половина)
             if (field[i][0].equals("X")) current = 1;
             else current = 0;
-            for (j = 1, m = i - 1; j < n && m >= 0; m--, j++) {
-                if (field[m][j].equals("X") && field[m + 1][j - 1].equals("X")) current += 1;
-                else {
+            for (j = 1, m = i - 1; j < desk && m >= 0; m--, j++) {
+                if (!field[m][j].equals("X")) {
                     if (current > max) max = current;
                     current = 0;
-                }
+                } else current += 1;
             }
             if (current > max) max = current;
         }
-        for (j = 1; j < n; j++) {               //вторая половина
-            if (field[n - 1][j].equals("X")) current = 1;
+        for (j = 1; j < desk; j++) {               //вторая половина
+            if (field[desk - 1][j].equals("X")) current = 1;
             else current = 0;
-            for (i = n - 2, m = j + 1; i >= 0 && m < n; i--, m++) {
+            for (i = desk - 2, m = j + 1; i >= 0 && m < desk; i--, m++) {
                 if (!field[i][m].equals("X")) {
                     if (current > max) max = current;
                     current = 0;
@@ -82,10 +82,10 @@ public class TicTacToe {
             }
             if (current > max) max = current;
         }
-        for (i = n - 1; i >= 0; i--) {               //проверка другой диагонали и || ей (первая половина)
+        for (i = desk - 1; i >= 0; i--) {               //проверка другой диагонали и || ей (первая половина)
             if (field[i][0].equals("X")) current = 1;
             else current = 0;
-            for (j = 1, m = i + 1; j < n && m < n ; m++, j++) {
+            for (j = 1, m = i + 1; j < desk && m < desk ; m++, j++) {
                 if (!field[m][j].equals("X")) {
                     if (current > max) max = current;
                     current = 0;
@@ -94,10 +94,10 @@ public class TicTacToe {
             }
             if (current > max) max = current;
         }
-        for (j = 1; j < n; j++) {                   //вторая половина
+        for (j = 1; j < desk; j++) {                   //вторая половина
             if (field[0][j].equals("X")) current = 1;
             else current = 0;
-            for (i = 0, m = j + 1; i >= 0 && m < n; i++, m++) {
+            for (i = 1, m = j + 1; i < desk && m < desk; i++, m++) {
                 if (!field[i][m].equals("X")) {
                     if (current > max) max = current;
                     current = 0;
@@ -109,20 +109,97 @@ public class TicTacToe {
         return max;
     }
 
+    public int checkZero() {           //тоже самое, что и checkCross, только вместо X - 0
+        int current, i, j, m;
+        int max = 0;
+        for (i = 0; i < desk; i++) {
+            if (field[i][0].equals("0"))
+                current = 1;
+            else current = 0;
+            for (j = 1; j < desk; j++)
+                if (!field[i][j].equals("0")) {
+                    if (current > max) max = current;
+                    current = 0;
+                }
+                else current += 1;
+            if (current > max) max = current;
+        }
+        if (max == 0) return max;
+        for (j = 0; j < desk; j++) {
+            if (field[0][j].equals("0")) current = 1;
+            else current = 0;
+            for (i = 1; i < desk; i++) {
+                if (!field[i][j].equals("0")) {
+                    if (current > max) max = current;
+                    current = 0;
+                } else current += 1;
+            }
+            if (current > max) max = current;
+        }
+        for (i = 0; i < desk; i++) {
+            if (field[i][0].equals("0")) current = 1;
+            else current = 0;
+            for (j = 1, m = i - 1; j < desk && m >= 0; m--, j++) {
+                if (!field[m][j].equals("0")) {
+                    if (current > max) max = current;
+                    current = 0;
+                } else current += 1;
+            }
+            if (current > max) max = current;
+        }
+        for (j = 1; j < desk; j++) {
+            if (field[desk - 1][j].equals("0")) current = 1;
+            else current = 0;
+            for (i = desk - 2, m = j + 1; i >= 0 && m < desk; i--, m++) {
+                if (!field[i][m].equals("0")) {
+                    if (current > max) max = current;
+                    current = 0;
+                }
+                else current += 1;
+            }
+            if (current > max) max = current;
+        }
+        for (i = desk - 1; i >= 0; i--) {
+            if (field[i][0].equals("0")) current = 1;
+            else current = 0;
+            for (j = 1, m = i + 1; j < desk && m < desk ; m++, j++) {
+                if (!field[m][j].equals("0")) {
+                    if (current > max) max = current;
+                    current = 0;
+                }
+                else current += 1;
+            }
+            if (current > max) max = current;
+        }
+        for (j = 1; j < desk; j++) {
+            if (field[0][j].equals("0")) current = 1;
+            else current = 0;
+            for (i = 1, m = j + 1; i < desk && m < desk; i++, m++) {
+                if (!field[i][m].equals("0")) {
+                    if (current > max) max = current;
+                    current = 0;
+                }
+                else current += 1;
+            }
+            if (current > max) max = current;
+        }
+        return max;
+    }
+
         public static void main (String[] args) {
-            TicTacToe demo = new TicTacToe(5);
-            demo.addCross(1, 2);
-            demo.addCross(2, 2);
-            demo.addCross(3, 2);
-            demo.addCross(4, 4);
-            demo.addCross(5, 5);
-            demo.addCross(3, 3);
-            demo.addCross(1, 5);
-            demo.addCross(2, 1);
-            demo.addCross(5,4);
-            demo.addCross(4, 5);
-            demo.show(5);
-            System.out.println(demo.checkCross(5));
+            TicTacToe game2 = new TicTacToe(6);
+            game2.addCross(2,2);
+            game2.addCross(3,2);
+            game2.addCross(5,2);
+            game2.addCross(6,2);
+            game2.addZero(4,2);
+            game2.addCross(4,3);
+            game2.addCross(4,4);
+            game2.addCross(4,5);
+            game2.addCross(5,4);
+            game2.addCross(6,5);
+            game2.show();
+            System.out.println(game2.checkCross());
         }
     }
 
--- tests/TicTacToeTest.java
+++ tests/TicTacToeTest.java
@@ -1,21 +1,118 @@
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 class TicTacToeTest {
+    /*@Override
+    public boolean equals(Object o) {
 
-    @Test
-    void show() {
-
-    }
-
+    }*/
     @Test
     void addZero() {
+        TicTacToe game = new TicTacToe(10);
+        game.addZero(10,2);
+        game.addZero(8, 1);
+        game.addZero(1, 3);
+        Assert.assertEquals("0", game.field[1][9]);
+        Assert.assertEquals("0", game.field[0][7]);
+        Assert.assertEquals("0", game.field[2][0]);
+        Assert.assertNotEquals("0", game.field[9][1]);
     }
 
     @Test
     void addCross() {
+        TicTacToe game = new TicTacToe(6);
+        game.addCross(4, 5);
+        game.addCross(4,6);
+        game.addCross(1,5);
+        Assert.assertEquals("X", game.field[4][3]);
+        Assert.assertEquals("X", game.field[5][3]);
+        Assert.assertEquals("X", game.field[4][0]);
+        Assert.assertNotEquals("X", game.field[3][3]);
     }
 
     @Test
     void delete() {
+        TicTacToe game = new TicTacToe(3);
+        game.addCross(1, 3);
+        game.addZero(2,2);
+        game.addCross(2,1);
+        game.delete(2,2);
+        Assert.assertEquals("_", game.field[1][1]);
+        Assert.assertEquals("X", game.field[2][0]);
+        game.delete(1,3);
+        Assert.assertEquals("_", game.field[2][0]);
+        game.delete(2,1);
+        Assert.assertEquals("_", game.field[0][1]);
+    }
+
+    @Test
+    void checkCross() {
+        TicTacToe game1 = new TicTacToe(3);
+        Assert.assertEquals(0, game1.checkCross()); //проверка на пустое поле
+        game1.addCross(1,1);
+        game1.addCross(2,3);
+        Assert.assertEquals(1, game1.checkCross()); //макс = 1
+        game1.addCross(3,3);
+        Assert.assertEquals(2, game1.checkCross()); //макс = 2, по горизонтали
+        TicTacToe game2 = new TicTacToe(6);
+        game2.addCross(2,2);
+        game2.addCross(3,2);
+        game2.addCross(5,2);
+        game2.addCross(6,2);
+        game2.addZero(4,2);
+        Assert.assertEquals(2, game2.checkCross()); //по горизонтали
+        game2.addCross(4,3);
+        game2.addCross(4,4);
+        game2.addCross(4,5);
+        Assert.assertEquals(3, game2.checkCross()); //по вертикали
+        game2.addCross(5,4);
+        game2.addCross(6,5);
+        Assert.assertEquals(4, game2.checkCross()); //по диагонали слева сверху вправо вниз (второй цикл)
+        TicTacToe game3 = new TicTacToe(10);
+        game3.addCross(3,10);
+        game3.addCross(2,9);
+        game3.addCross(1, 8);
+        Assert.assertEquals(3, game3.checkCross()); //по диагонали слева сверху вправо вниз (первый цикл)
+        game3.addCross(1, 4);
+        game3.addCross(2, 3);
+        game3.addCross(3, 2);
+        game3.addCross(4, 1);
+        Assert.assertEquals(4, game3.checkCross()); //по диагонали слева снизу вправо вверх (первый цикл)
+        game3.addCross(5, 8);
+        game3.addCross(6, 7);
+        game3.addCross(7, 6);
+        game3.addCross(8, 5);
+        game3.addCross(9, 4);
+        Assert.assertEquals(5, game3.checkCross()); //по диагонали слева снизу вправо вверх (второй цикл)
+    }
+
+    @Test
+    void checkZero() {
+        TicTacToe game = new TicTacToe(10);
+        game.addZero(4,5);
+        game.addZero(5,5);
+        game.addZero(6,5);
+        Assert.assertEquals(3, game.checkZero()); //горизонталь
+        game.addZero(6,7);
+        game.addZero(6,6);
+        game.addZero(6,4);
+        Assert.assertEquals(4, game.checkZero()); //вертикаль
+        game.delete(6, 5);
+        game.addZero(7,9);
+        game.addZero(8,8);
+        game.addZero(9,7);
+        Assert.assertEquals(3, game.checkZero()); //по диагонали слева снизу вправо вверх (второй цикл)
+        game.addZero(3,7);
+        game.addZero(4,6);
+        Assert.assertEquals(4, game.checkZero()); //по диагонали слева снизу вправо вверх (первый цикл)
+        game.addZero(7,7);
+        game.addZero(4,4);
+        Assert.assertEquals(5, game.checkZero()); //по диагонали слева свехру вправо вниз (первый цикл)
+        game.addCross(5,5);
+        game.addZero(9, 5);
+        game.addZero(8, 4);
+        game.addZero(7, 3);
+        game.addZero(6, 2);
+        Assert.assertEquals(4, game.checkZero()); //по диагонали слева свехру вправо вниз (второй цикл)
     }
 }
